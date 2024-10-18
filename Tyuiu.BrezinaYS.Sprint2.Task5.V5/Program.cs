8e367811c19eec75da0a5ca13d64afaba9918ac1
ulabrezina <ulabrezina@gmail.com>
2024-10-18 23:55:41 +0500

    Task5

---
+++ Tyuiu.BrezinaYS.Sprint2.Task5.V5/Program.cs
@@ -0,0 +1,45 @@
+﻿using Tyuiu.BrezinaYS.Sprint2.Task5.V5.Lib;
+namespace Tyuiu.BrezinaYS.Sprint2.Task5.V5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #2 | Выполнил: Брезина Ю. С. | ИИПб-24-2";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Оператор switch                                                   *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #5                                                              *");
+            Console.WriteLine("* Выполнил: Брезина Юлия Сергеевна | ИИПб-24-2                            *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая использует оператор switch,                 *");
+            Console.WriteLine("* вычисляет требуемое значение и возвращает результат                     *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите номер карты: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            string res;
+            if ((x < 6) || (x > 14))
+            {
+                res = "Введенно неверное значение!";
+            }
+            else
+            {
+                res = "Это карта: " + ds.FindCardValue(x);
+            }
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                            *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(res);
+            Console.ReadKey();
+
+        }
+    }
+}
